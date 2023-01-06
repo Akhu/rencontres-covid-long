4da93ec16e9b8a85dc382611110033373f25c7a9
Anthony Da Cruz <me@anthony-dacruz.com>
2023-01-06 21:48:30 +0100

    Updated first meeting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import '../app.css'
+  import arnaud from '$lib/images/arnaud.png'
+  import anthony from '$lib/images/anthony.jpg'
 </script>
 
 <div class="app bg-base-200">
@@ -10,11 +12,26 @@
   <footer class="footer p-10 bg-base-300 text-base-content mt-10">
     <div>
       <span class="footer-title">Qui sommes nous ?</span>
-      <p>Arnaud et Anthony, nous sommes deux malades du Covid Long, Arnaud depuis Avril 2020, et Anthony depuis Novembre 2021. Notre objectif est simplement de partager notre experience avec d'autres malades en offrant un minimum d'organisation et un lieu de rencontre.</p>
+      <div class="flex flex-row">
+        <div class="avatar-group -space-x-4">
+          <div class="avatar">
+            <div class="w-14">
+              <img src={anthony} alt="Anthony" />
+
+            </div>
+          </div>
+          <div class="avatar">
+            <div class="w-14">
+              <img src={arnaud} alt="Arnaud" />
+            </div>
+          </div>
+        </div>
+      <p class="pl-4">Arnaud et Anthony, nous sommes simplement deux malades du Covid Long, Arnaud depuis Avril 2020, et Anthony depuis Novembre 2021. Notre objectif est simplement de partager notre experience avec d'autres malades en offrant un minimum d'organisation et un lieu de rencontre.</p>
+      </div>
     </div>
     <div>
-      <span class="footer-title">Des liens</span>
-      <a class="link link-hover" href="mailto:azzouz.arnaud@gmail.com">✉️ Nous contacter par mail</a>
+      <span class="footer-title">Nous contacter en cas de besoin</span>
+      <a class="link link-hover" href="mailto:azzouz.arnaud@gmail.com">✉️ Contacter Arnaud par mail</a>
       <a class="link link-hover" href="https://metacovidlong.substack.com"> 📥 La newsletter - Meta Covid Long</a>
       <a class="link link-hover" href="https://twitter.com/anthokhun"> 🐥Contacter Anthony sur Twitter</a>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import heroImage from "$lib/images/hero-image.png"
   import nextDateImage from "$lib/images/next-date-image.png"
+  import papeteriesImage from "$lib/images/papet.jpeg"
 </script>
 
 <section>
@@ -25,35 +26,41 @@
       <div>
         <h2 class="text-2xl font-extrabold">
           <div class="badge badge-sm badge-error shadow-lg shadow-red-500 mr-2"></div>
-          Prochaine date
+          Prochaine date : 26/01/2023
         </h2>
         <div class="card bg-base-100 shadow-xl md:pr-0 mt-4 flex flex-col">
           <div class="flex flex-row md:px-8">
-            <figure class="hidden"><img alt="Coucher de soleil sur le lac d'Annecy" class="hidden md:grid w-68 rounded-2xl bg-primary text-primary-content place-content-center" src={nextDateImage}/></figure>
+            <figure class="hidden">
+              <img alt="Les Papeteries Images Factory Immeuble de bureaux" class="grid w-68 rounded-2xl bg-primary text-primary-content place-content-center" width="350" src={papeteriesImage}/>
+            </figure>
             <div class="card-body">
               <h2 class="card-title bg-base-200 rounded-full py-2 max-w-fit px-4 "><span class="material-symbols-rounded">location_on</span>Annecy, Haute-Savoie</h2>
               <p class="prose">Première édition du groupe de parole : <b>Ouvert à tous</b></p>
               <div class="px-6">
                   <ul class="list-disc prose">
-                    <li>(Date précise à définir)</li>
-                    <li>(Horaire à définir)</li>
-                    <li>(Lieu à définir)</li>
+                    <li>Le 26 Janvier 2023</li>
+                    <li>Dès 14h</li>
+                    <li>Les Papeteries Images Factory,
+                      1 Esplanade Augustin Aussedat,
+                      74960, Annecy <br/>
+                      Salle de Réunion (RDC)
+                    </li>
+                    <li><a href="https://www.lespapeteries.com/">Informations pratiques (parking, bus etc.)</a></li>
                   </ul>
               </div>
             </div>
           </div>
-          <div class="prose pb-6 min-w-fit leading-normal px-8" id="how">
-            <h3><span class="text-amber-500 material-symbols-rounded">commute</span> Comment venir ? </h3>
+          <div class="prose pb-6 min-w-fit leading-normalpx-8" id="how">
+            <h2><span class="text-amber-500 material-symbols-rounded">commute</span> Comment venir ? </h2>
+            <h3>1. Se rendre à Annecy </h3>
             <ul>
               <li>Par le train, la gare d’Annecy est bien desservie par les TER et TGV également depuis les grandes villes</li>
               <li>En bus, la gare routière d’Annecy est dense et bien desservie (FlixBus, BlaBla, Kelbus, etc.)</li>
               <li>Pensez à Blabla Car également</li>
             </ul>
             <div class="flex flex-col md:flex-row items-center gap-2 px-8"><a class="btn-outline btn" href="https://www.sncf-connect.com/train/france/auvergne-rhone-alpes/annecy">Reserver un billet de train vers Annecy</a><a class="btn-outline btn" href="https://www.blablacar.fr/">BlaBlaCar</a></div>
-            <h4> 🏔️ Une fois sur Annecy </h4>
-            <p>Depuis la gare d’Annecy, prenez le BUS 3 direction Epagny et sortir à l’arret Vignère.
-              Pensez à installer l’application Sibra, Google Maps fonctionne bien également avec les lignes</p>
-            <div class="flex flex-col md:flex-row items-center gap-2"><a href="" class="btn-outline btn">Voir sur Google Maps (a définir)</a><a class="btn-outline btn" href="https://www.sibra.fr/">Site de la Sibra</a></div>
+            <h3>2. Une fois sur Annecy </h3>
+            <div class="flex flex-col md:flex-row items-center gap-2"><a class="btn-accent btn" href="https://www.lespapeteries.com/">Instructions pour se rendre sur le lieu (parking, bus etc.)</a><a href="https://goo.gl/maps/uFskPZAbnh6ka23S7" class="btn-outline btn">Voir sur Google Maps</a><a class="btn-outline btn" href="https://www.sibra.fr/">Site de la Sibra</a></div>
 
           </div>
         </div>
@@ -63,7 +70,7 @@
       <div class="flex flex-row mt-12 md:px-0" id="about">
         <div class="prose min-w-full ">
           <h2>En quoi cela consiste ?</h2>
-          <p>Une simple rencontre pour partager notre condition de malade, se sentir moins seul. Se rendre compte que cela n'arrive pas qu'à soi et donc se remonter le moral, partager des conseils et des solutions.</p>
+          <p>Une simple rencontre pour partager notre condition de malade, se sentir moins seul. Se rendre compte que cela n'arrive pas qu'à soi, partager conseils et solutions pour soulager la maladie.</p>
           <blockquote>
             « Lorsque vous vous sentez déprimé, l'idée d'affronter une conversation avec quelqu'un peut sembler épuisante et accablante. C'est le piège de la dépression induite par la maladie. On a envie de se retirer, de se cacher, de ne voir personne jusqu'à se sentir mieux.
             Alors on attend de se sentir mieux. Mais ce faisant, nous nous retenons.
