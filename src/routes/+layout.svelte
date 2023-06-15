bd100598122a1c7760b3fda33f1705e3975a3e75
Anthony Da Cruz <me@anthony-dacruz.com>
2023-06-15 17:48:56 +0200

    Updated June 2023

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,37 +4,41 @@
   import anthony from '$lib/images/anthony.jpg'
 </script>
 
-<div class="app bg-base-200 ">
-	<main class="container mx-auto w-screen">
+<div class="app bg-base-100">
+	<main class="container mx-auto max-w-6xl px-4">
 		<slot />
 	</main>
 
-  <footer class="footer p-10 bg-base-300 text-base-content mt-10">
-    <div>
-      <span class="footer-title">Qui sommes nous ?</span>
-      <div class="avatar-group -space-x-4">
-        <div class="avatar">
-          <div class="w-14 h-14">
-            <img src={anthony} alt="Anthony" />
-
-          </div>
-        </div>
-        <div class="avatar">
-          <div class="w-14 h-14">
-            <img src={arnaud} alt="Arnaud" />
-          </div>
-        </div>
+  <footer class="px-6 py-9 bg-base-300 mt-10">
+    <div class="container mx-auto w-screen max-w-6xl">
+      <div class="text-base-content flex lg:flex-row flex-col space-x-0 lg:space-x-4 space-y-10 lg:space-y-0">
+      <div class="flex flex-col flex-grow w-3/5">
+        <h1 class="footer-title" id="contact">Nous contacter</h1>
+        <a class="link link-hover" href="mailto:azzouz.arnaud@gmail.com">✉️ Contacter Arnaud par mail</a>
+        <a class="link link-hover" href="https://metacovidlong.substack.com"> 📥 La newsletter - Meta Covid Long</a>
+        <a class="link link-hover" href="https://twitter.com/anthokhun"> 🐥Contacter Anthony sur Twitter</a>
       </div>
-      <div class="flex flex-row">
+      <div class="flex flex-col flex-shrink flex-grow-0">
+        <span class="footer-title">Qui sommes nous ?</span>
+        <div class="avatar-group -space-x-4">
+          <div class="avatar">
+            <div class="w-14 h-14">
+              <img src={anthony} alt="Anthony" />
 
-      <p class="pl-4">Arnaud et Anthony, nous sommes simplement deux malades du Covid Long, Arnaud depuis Avril 2020, et Anthony depuis Novembre 2021. Notre objectif est simplement de partager notre experience avec d'autres malades en offrant un minimum d'organisation et un lieu de rencontre.</p>
+            </div>
+          </div>
+          <div class="avatar">
+            <div class="w-14 h-14">
+              <img src={arnaud} alt="Arnaud" />
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row">
+
+        <p>Arnaud et Anthony, nous sommes simplement deux malades du Covid Long, Arnaud depuis Avril 2020, et Anthony depuis Novembre 2021. Notre objectif est simplement de partager notre experience avec d'autres malades en offrant un minimum d'organisation et un lieu de rencontre.</p>
+        </div>
       </div>
     </div>
-    <div>
-      <span class="footer-title">Nous contacter en cas de besoin</span>
-      <a class="link link-hover" href="mailto:azzouz.arnaud@gmail.com">✉️ Contacter Arnaud par mail</a>
-      <a class="link link-hover" href="https://metacovidlong.substack.com"> 📥 La newsletter - Meta Covid Long</a>
-      <a class="link link-hover" href="https://twitter.com/anthokhun"> 🐥Contacter Anthony sur Twitter</a>
     </div>
   </footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import heroImage from "$lib/images/hero-image.png"
   import papeteriesImage from "$lib/images/papet.jpeg"
+  import metaCovidLong from "$lib/images/meta-covidlong-logo.png"
 </script>
 
 <section>
@@ -10,7 +11,7 @@
         <img src={heroImage} alt="Personnes écoutants lors d'une conférence" class="w-28 hidden md:block lg:w-40"/>
         <div>
           <h1 class="text-5xl font-bold">Rencontres Covid Long</h1>
-          <p class="pb-6 text-xl">Nous organisons des sessions de rencontres entre malades et concernés (proches, amis de malades) <br/>
+          <p class="py-6 text-xl">Nous organisons des sessions de rencontres entre malades et concernés (proches, amis de malades) <br/>
             S’entraider, se soutenir et partager les expériences de chacun pour ne plus affronter la maladie seul. </p>
           <div class="flex flex-col md:flex-row gap-4">
             <a href="#how" class="btn btn-primary">Comment participer ?</a>
@@ -20,12 +21,88 @@
       </div>
     </div>
     <div class="divider"></div>
-    <div class="flex flex-col min-h-full p-4">
+    <div class="my-8">
+      <h2 class="text-3xl font-extrabold my-4">
+        <div class="badge badge-sm badge-error shadow-lg mr-2"></div>
+        Prochaine date : à définir...
+      </h2>
+      <p>Hésitez pas à nous <a class="link-hover btn-link" href="#contact">contacter</a> pour manifester votre intérêt.</p>
+    </div>
+    <div class="flex flex-col min-h-full my-8">
       <!-- Content Next Date-->
       <div>
+        <h1 class="text-3xl font-black mb-6">Vous n'êtes pas seuls ! Des infos sur le Covid Long ⬇️</h1>
+        <div class="flex flex-col mb-4">
+          <div class="flex flex-col lg:flex-row lg:space-x-8 lg:space-y-0 space-y-4 space-x-0 items-stretch">
+            <div class="card bg-secondary shadow-md flex flex-col">
+              <div class="card-body">
+                <div class="card-title font-black">Groupe de soutiens et de rencontres</div>
+                <div class="flex flex-col items-start space-y-4">
+                <div class="flex flex-col-reverse items-stretch sm:flex-row sm:items-center sm:space-x-3 space-x-0">
+                  <a class="btn btn-outline" href="https://www.facebook.com/groups/913274646358443/?ref=share_group_link">Consulter</a><span>Groupe Facebook de solidarité pour les malades du Covid Long</span>
+                </div>
+                <div class="flex flex-col-reverse items-stretch sm:flex-row sm:items-center sm:space-x-3 space-x-0">
+                  <a class="btn btn-outline" href="https://www.facebook.com/groups/962932755081307/?ref=share_group_link">Consulter</a><span>Groupe Facebook de rencontre physique entre malades du Covid Long</span>
+                </div>
+              </div>
+              </div>
+            </div>
+            <div class="card bg-primary shadow-md flex flex-col flex-grow">
+              <div class="card-body">
+                <div class="card-title font-black">Associations</div>
+                <div class="flex flex-row space-x-3 ">
+                  <a class="btn btn-outline" href="https://winslowsantepublique.wordpress.com/">Winslow Santé Publique</a>
+                </div>
+                <div class="flex flex-row space-x-3 ">
+                  <a class="btn btn-outline" href="https://www.helloasso.com/associations/covid-long-solidarite/">Covid Long Solidarité</a>
+                </div>
+                <div class="flex flex-row items-center space-x-3">  
+                  <a class="btn btn-outline" href="https://www.apresj20.fr/accueil">Après J20</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col lg:flex-row lg:space-x-8 space-y-6 lg:align-top lg:items-center">
+            <div class="card lg:card-side bg-accent text-white shadow-md mt-4 flex flex-col">
+              <figure class="w-1/5">
+                <img  src="{metaCovidLong}" alt="logo meta covid long" class="ml-8 rounded-xl h-12 w-12 aspect-square" />
+              </figure>
+              <div class="card-body">
+                <div class="card-title font-black">Blog : Meta Covid Long</div>
+                  <div class="flex flex-col space-y-4 items-start">
+                    <span>Reflexions sur le Covid Long. Structurer et diffuser des informations scientifiques et objectives sur le Covid Long, mais aussi les problèmes sociaux et sociétaux provoqués par le Covid Long, par un malade du Covid Long</span>
+                    <a class="btn-outline btn flex-shrink text-white" href="https://metacovidlong.substack.com">Consulter</a>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+            
+          </div>
+      </div>
+      <h3 class="text-lg text-slate-500 mb-6">Vous avez une ressource à partager pour les Covid Long ? <a class="btn-link link-primary" href="">Contactez moi sur Twitter</a>, je l'ajouterai ici</h3>
+
+      <!-- Content Instructions -->
+      <div class="flex flex-row my-8 md:px-0" id="about">
+        <div class="prose min-w-full ">
+          <h2 class="text-3xl">Pourquoi se connecter avec d'autres malades ?</h2>
+          <p>Une simple rencontre pour partager notre condition de malade, se sentir moins seul. Se rendre compte que cela n'arrive pas qu'à soi, partager conseils et solutions pour soulager la maladie.</p>
+          <blockquote>
+            « Lorsque vous vous sentez déprimé, l'idée d'affronter une conversation avec quelqu'un peut sembler épuisante et accablante. C'est le piège de la dépression induite par la maladie. On a envie de se retirer, de se cacher, de ne voir personne jusqu'à se sentir mieux.
+            Alors on attend de se sentir mieux. Mais ce faisant, nous nous retenons.
+            Prendre du temps pour être seul peut être réénergisant, mais cela peut aussi très facilement devenir une spirale vicieuse de rumination et de dégoût de soi qui alimente la dépression et la maintient. » <br/>
+
+            « Le soutien social a des effets positifs à la fois pour celui qui le reçoit et pour celui qui le fournit (Inagaki et al., 2012). »
+          </blockquote>
+          <p class="font-semibold text-sm">Extrait de
+            Why Has Nobody Told Me This Before?
+            Dr Julie Smith</p>
+        </div>
+      </div>
+      <div class="archive my-9">
         <h2 class="text-2xl font-extrabold">
-          <div class="badge badge-sm badge-error shadow-lg shadow-red-500 mr-2"></div>
-          Prochaine date : 26/01/2023
+          <div class="badge badge-sm badge-secondary shadow-lg mr-2"></div>
+          La dernière rencontre, c'était le 26/01/2023 (Merci aux participants !)
         </h2>
         <div class="card bg-base-100 shadow-xl md:pr-0 mt-4 flex flex-col">
           <div class="flex flex-col md:flex-row md:px-8">
@@ -34,7 +111,7 @@
             </figure>
             <div class="card-body">
               <h2 class="card-title bg-base-200 rounded-full py-2 max-w-fit px-4 "><span class="material-symbols-rounded">location_on</span>Annecy, Haute-Savoie</h2>
-              <p class="prose">Première édition du groupe de parole : <b>Ouvert à tous</b></p>
+              <p class="prose">Groupe de parole : <b>Ouvert à tous</b></p>
               <div class="px-6">
                   <ul class="list-disc prose">
                     <li>Le 26 Janvier 2023</li>
@@ -69,26 +146,7 @@
 
           </div>
         </div>
-
       </div>
-      <!-- Content Instructions -->
-      <div class="flex flex-row mt-12 md:px-0" id="about">
-        <div class="prose min-w-full ">
-          <h2>En quoi cela consiste ?</h2>
-          <p>Une simple rencontre pour partager notre condition de malade, se sentir moins seul. Se rendre compte que cela n'arrive pas qu'à soi, partager conseils et solutions pour soulager la maladie.</p>
-          <blockquote>
-            « Lorsque vous vous sentez déprimé, l'idée d'affronter une conversation avec quelqu'un peut sembler épuisante et accablante. C'est le piège de la dépression induite par la maladie. On a envie de se retirer, de se cacher, de ne voir personne jusqu'à se sentir mieux.
-            Alors on attend de se sentir mieux. Mais ce faisant, nous nous retenons.
-            Prendre du temps pour être seul peut être réénergisant, mais cela peut aussi très facilement devenir une spirale vicieuse de rumination et de dégoût de soi qui alimente la dépression et la maintient. » <br/>
-
-            « Le soutien social a des effets positifs à la fois pour celui qui le reçoit et pour celui qui le fournit (Inagaki et al., 2012). »
-          </blockquote>
-          <span class="badge badge-outline">Extrait de
-            Why Has Nobody Told Me This Before?
-            Dr Julie Smith</span>
-        </div>
-      </div>
-
     </div>
 
   </div>
